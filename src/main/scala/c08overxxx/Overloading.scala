f5409d07b8d58e14daa94808d5d8cf4cc17cebfd
matia storo <mtoro.cl@gmail.com>
2025-09-03 16:09:54 -0400

    clase overloading

--- src/main/scala/c08overxxx/Overloading.scala
+++ src/main/scala/c08overxxx/Overloading.scala
@@ -3,10 +3,10 @@ package c08overxxx
 object Overloading:
 
   class Animal:
-    def greet(a: Cat) = println("Animal.greet")
+    def greet(a: Cat): Unit = println("Animal.greet")
 
   class Cat extends Animal:
-    def greet(c: Animal) = println("Cat.greet")
+    def greet(c: Animal): Unit = println("Cat.greet")
 
   val cat1: Animal = new Cat()
   val cat2: Cat = new Cat()
